a317d7cdc878c99c5f6b1fbe4dadc91780f4be31
datcog1111 <h9tuck@gmail.com>
2023-05-26 21:57:05 -0500

    created forms to add and update both authors and books

--- utils/startApp.js
+++ utils/startApp.js
@@ -4,6 +4,10 @@ import navBar from '../components/shared/navBar';
 import domEvents from '../events/domEvents';
 import formEvents from '../events/formEvents';
 import navigationEvents from '../events/navigationEvents';
+import { getBooks } from '../api/bookData';
+import { showBooks } from '../pages/books';
+import { showAuthors } from '../pages/authors';
+import { getAuthors } from '../api/authorData';
 
 const startApp = () => {
   domBuilder(); // BUILD THE DOM
@@ -12,6 +16,8 @@ const startApp = () => {
   navBar(); // DYNAMICALLY ADD THE NAV
   logoutButton(); // ADD THE LOGOUT BUTTON COMPONENT
   navigationEvents(); // ATTACH THE EVENT LISTENERS TO THE NAVBAR
+  getBooks().then((books) => showBooks(books));
+  getAuthors().then((authors) => showAuthors(authors));
 
   // TODO: Put all books on the DOM on App load
 };
